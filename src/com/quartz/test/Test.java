f5a03d67a472012e1e1a8e93349ac03fecfef3e2
yangjianzhou <yangjianzhouctgu@126.com>
2014-11-21 21:04:00 +0800

    add quartz test

---
+++ src/com/quartz/test/Test.java
@@ -0,0 +1,15 @@
+package com.quartz.test;
+
+/**
+ * 
+ * @author : yangjianzhou
+ * @description : TODO
+ * @time : Nov 21, 2014,9:02:30 PM
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
